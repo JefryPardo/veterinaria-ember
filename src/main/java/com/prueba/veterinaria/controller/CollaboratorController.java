bce9d4ed4ef1815f350787d2a0b5291c0369d7d6
Jefry Pardo <jeffry.pardo01@unicatolica.edu.co>
2021-10-15 08:08:56 -0500

    segundo commit

--- src/main/java/com/prueba/veterinaria/controller/CollaboratorController.java
+++ src/main/java/com/prueba/veterinaria/controller/CollaboratorController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/collaborator")
+@RequestMapping("/vet/collaborator")
 public class CollaboratorController {
     
     @Autowired
